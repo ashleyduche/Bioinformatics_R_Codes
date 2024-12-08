159f44a99db3ae950258f4626d4498e4b07c0311
ashleyduche <71372500+ashleyduche@users.noreply.github.com>
2024-12-07 21:19:47 -0800

    Delete check_and_install_packages.R

--- check_and_install_packages.R
+++
@@ -1,8 +0,0 @@
-# check and install packages
-getpkg <- function(package) {
-    if (!require(package, character.only = TRUE)) {
-        install.packages(package)
-        library(package,character.only = TRUE)
-    }
-}
-getpkg("")
